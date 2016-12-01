b392b78969a072b0192f004e5bf2fe335c189f07
Marcel <MTRNord@users.noreply.github.com>
2016-12-01 13:48:25 +0100

    Fix Func

--- hangupsbot/plugins/telesync/pluginAPI.py
+++ hangupsbot/plugins/telesync/pluginAPI.py
@@ -8,8 +8,8 @@ class Event(object):
         
 class Bot(object):
     # The class "constructor" - It's actually an initializer 
-    def __init__(self, coro_send_message):
-        self.coro_send_message = coro_send_message
+    def __init__(self, bot):
+        setattr(self, 'coro_send_message', bot.sendMessage)
 
 def tg_command_register(bot, cmd, shared_func):
     global commands
@@ -19,7 +19,7 @@ def tg_command_register(bot, cmd, shared_func):
 def tg_command_wrapper(bot, chat_id, args, cmd):
     params = args['params'][0]
     event = Event(chat_id, chat_id)
-    ho_bot = Bot(bot.sendMessage(chat_id, msg))
+    ho_bot = Bot(bot)
     tg2ho_dict = bot.ho_bot.memory.get_by_path(['telesync'])['tg2ho']
     if str(chat_id) in tg2ho_dict:
         ho_conv_id = tg2ho_dict[str(chat_id)]
