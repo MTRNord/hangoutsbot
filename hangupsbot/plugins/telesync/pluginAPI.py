c8f91e715b4edaa233b53635a8fe5a3c057b4bcf
Marcel <MTRNord@users.noreply.github.com>
2016-12-01 12:34:14 +0100

    Add sendMessage

--- hangupsbot/plugins/telesync/pluginAPI.py
+++ hangupsbot/plugins/telesync/pluginAPI.py
@@ -5,6 +5,11 @@ class Event(object):
     def __init__(self, conv_id, conv):
         self.conv_id = conv_id
         self.conv = conv
+        
+class Bot(object):
+    # The class "constructor" - It's actually an initializer 
+    def __init__(self, coro_send_message):
+        self.coro_send_message = coro_send_message
 
 def tg_command_register(bot, cmd, shared_func):
     global commands
@@ -14,7 +19,7 @@ def tg_command_register(bot, cmd, shared_func):
 def tg_command_wrapper(bot, chat_id, args, cmd):
     params = args['params'][0]
     event = Event(chat_id, chat_id)
-
+    ho_bot = Bot(bot.sendMessage(chat_id, msg))
     tg2ho_dict = bot.ho_bot.memory.get_by_path(['telesync'])['tg2ho']
     if str(chat_id) in tg2ho_dict:
         ho_conv_id = tg2ho_dict[str(chat_id)]
