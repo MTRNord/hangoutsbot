983302104b3b0709460895dbb9841489a46ee28c
Marcel <MTRNord@users.noreply.github.com>
2016-12-01 11:22:59 +0100

    Update pluginAPI.py

--- hangupsbot/plugins/telesync/pluginAPI.py
+++ hangupsbot/plugins/telesync/pluginAPI.py
@@ -11,10 +11,10 @@ def tg_command_wrapper(bot, chat_id, args, cmd):
     tg2ho_dict = bot.ho_bot.memory.get_by_path(['telesync'])['tg2ho']
     if str(chat_id) in tg2ho_dict:
         ho_conv_id = tg2ho_dict[str(chat_id)]
-        tldr_args = {'params': params, 'conv_id': ho_conv_id}
         global commands
+        
         try:
-            text = bot.ho_bot.call_shared(commands[cmd], bot.ho_bot, tldr_args)
+            yield from commands[cmd](bot.ho_bot, null, params)
             yield from bot.sendMessage(chat_id, text, parse_mode='HTML')
         except KeyError as ke:
             yield from bot.sendMessage(chat_id, "{cmd} plugin is not active. KeyError: {e}".format(e=ke, cmd=cmd))
