14eaa4ad00388edc13b505b8ff33d0a69ae844a6
Marcel <MTRNord@users.noreply.github.com>
2016-12-01 12:09:22 +0100

    Fix Event

--- hangupsbot/plugins/telesync/pluginAPI.py
+++ hangupsbot/plugins/telesync/pluginAPI.py
@@ -1,12 +1,19 @@
 commands = {}
 
+class Event(object):
+    # The class "constructor" - It's actually an initializer 
+    def __init__(self, conv_id, conv):
+        self.conv_id = conv_id
+        self.conv = conv
+
 def tg_command_register(bot, cmd, shared_func):
     global commands
     commands[cmd] = shared_func
     bot.call_shared("telesync.add_command", cmd, tg_command_wrapper)
 
 def tg_command_wrapper(bot, chat_id, args, cmd):
-    params = args['params']
+    params = args['params'][0]
+    event = Event(chat_id, chat_id)
 
     tg2ho_dict = bot.ho_bot.memory.get_by_path(['telesync'])['tg2ho']
     if str(chat_id) in tg2ho_dict:
@@ -14,7 +21,7 @@ def tg_command_wrapper(bot, chat_id, args, cmd):
         global commands
         
         try:
-            yield from commands[cmd](bot.ho_bot, null, params)
+            yield from commands[cmd](bot.ho_bot, event, params)
             yield from bot.sendMessage(chat_id, text, parse_mode='HTML')
         except KeyError as ke:
             yield from bot.sendMessage(chat_id, "{cmd} plugin is not active. KeyError: {e}".format(e=ke, cmd=cmd))
