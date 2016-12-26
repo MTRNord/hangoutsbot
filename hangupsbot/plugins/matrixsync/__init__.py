25e5b5a9e57c877d1dbc2f086a96963c3a87f191
Marcel <MTRNord@users.noreply.github.com>
2016-12-26 01:06:09 +0100

    Fix missing line

--- hangupsbot/plugins/matrixsync/__init__.py
+++ hangupsbot/plugins/matrixsync/__init__.py
@@ -100,6 +100,7 @@ def on_message(self, event):
             mx_on_message(mx_chat_alias, msg, roomName, user)
     elif event['type'] == "m.room.message":
         if event['content']['msgtype'] == "m.text":
+            user_obj = matrix_bot.get_user(event['sender'])
             user = user_obj.get_display_name()
             roomName = matrix_bot.get_room_name(event['room_id'])
             msg = event['content']['body']
