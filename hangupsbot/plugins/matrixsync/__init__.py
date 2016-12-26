a64a7edd31a8180eab325d100edeeae2fdcc5fbe
Marcel <MTRNord@users.noreply.github.com>
2016-12-26 16:59:21 +0100

    Add message formatting

--- hangupsbot/plugins/matrixsync/__init__.py
+++ hangupsbot/plugins/matrixsync/__init__.py
@@ -245,16 +245,14 @@ def _on_hangouts_message(bot, event, command=""):
 
     ho2mx_dict = bot.memory.get_by_path(['matrixsync'])['ho2mx']
 
-    print(event.conv_id)
-
     if event.conv_id in ho2mx_dict:
         try:
             room = matrix_bot.join_room(ho2mx_dict[event.conv_id])
 
             user_gplus = 'https://plus.google.com/u/0/{uid}/about'.format(uid=event.user_id.chat_id)
-            text = '"{uname}": {text}'.format(uname=event.user.full_name,
-                                              text=sync_text)
-            room.send_text(text)
+            text_plain = '{uname}: {text}'.format(uname=event.user.full_name, text=sync_text)
+            text_html = '<a href="{user_gplus}">{uname}</a> <b>({gname})</b>: {text}'.format(uname=event.user.full_name, text=sync_text, user_gplus=user_gplus, gname=event.conv.name)
+            matrix_bot.api.send_message_event(ho2mx_dict[event.conv_id], "m.room.message", {"msgtype": "m.text", "body": text_plain, "formatted_body": text_html,  "format": "org.matrix.custom.html"})
 
             if has_photo:
                 logger.info("plugins/matrixsync: photo url: {url}".format(url=photo_url))
